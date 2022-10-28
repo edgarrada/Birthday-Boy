fe0b8d76982eb5047fb00c9770bd3958ecd621af
edgarrada <116873249+edgarrada@users.noreply.github.com>
2022-10-28 22:51:45 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// img2
+
+function img6(){
+    document.querySelector('#img6')
+    .addEventListener('click', () => {
+        window.location.href = 'tabibee.html'
+    })
+}
+
+function manages(){
+    document.getElementById("manages").classList.toggle("active")
+}
